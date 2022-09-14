c56fbfa87b69d4cb88d7f51e957bbee37d368e49
Evgeniy Azarov <azarov.private@gmail.com>
2022-09-15 01:20:50 +0300

    Add template files for src folder

---
+++ cra-template-base/template/src/App.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+
+import App from './App'
+
+test('renders `hello app`', () => {
+  render(<App />)
+
+  const linkElement = screen.getByText(/hello app/i)
+
+  expect(linkElement).toBeInTheDocument()
+})
